32032ad3825e3715d1ce5daee4d120542482f7f1
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-15 15:30:59 +0530

    Scrape data and store in object

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express')
-const fs = require('fs')
+// const fs = require('fs')
 const cheerio = require('cheerio')
+const request = require('request')
 const app = express()
+const PORT = 9191
 
-app.get('/', () => {
+app.get('/', (req, res) => {
+  let url = 'http://www.imdb.com/title/tt1229340/'
+  request(url, (err, response, html) => {
+    if (!err) {
+      let $ = cheerio.load(html)
+      let title, release, rating
+      let json = {title: '', release: '', rating: ''}
+      title = $('.title_wrapper h1').text()
+      json.title = title
+      release = $('.title_wrapper h1 span a').text()
+      json.release = release
+      rating = $('.ratingValue strong span').text()
+      json.rating = rating
+      console.log(json)
+    }
+  })
+})
 
-} )
-
-app.listen(8181, () => {
-  console.log('Listening on port: 8181')
+app.listen(PORT, () => {
+  console.log('Listening on port: ' + PORT)
 })
