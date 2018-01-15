dfb062a049993786ffabd237d445c6d45ff2881d
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-15 20:15:15 +0530

    Pass movie id as query params and call scraper function

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 const express = require('express')
-// const fs = require('fs')
-const cheerio = require('cheerio')
-const request = require('request')
+const url = require('url')
 const app = express()
+const scraper = require('./lib/scraper')
 const PORT = 9191
 
-app.get('/', (req, res) => {
-  let url = 'http://www.imdb.com/title/tt1229340/'
-  request(url, (err, response, html) => {
-    if (!err) {
-      let $ = cheerio.load(html)
-      let title, release, rating
-      let json = {title: '', release: '', rating: ''}
-      title = $('.title_wrapper h1').text()
-      json.title = title
-      release = $('.title_wrapper h1 span a').text()
-      json.release = release
-      rating = $('.ratingValue strong span').text()
-      json.rating = rating
-      console.log(json)
-    }
-  })
+app.use(express.static('public'))
+
+app.get('/movie', (req, res) => {
+  scraper.scrapeMovie(url.parse(req.url, true).query.id, res)
 })
 
+app.get('/top', (req, res) => {
+  let url = 'http://www.imdb.com/chart/top?ref_=nv_mv_250_6'
+  scraper.scrapeTopMovies(url, res)
+})
 app.listen(PORT, () => {
   console.log('Listening on port: ' + PORT)
 })
