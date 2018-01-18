d921173c3b54644de0f327481da9026e519fbf60
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-18 08:00:55 +0530

    Separate out db insertion logic

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const pg = require('pg')
-const app = express()
 const config = require('./config')
-const helper = require('./lib/helper')
-const populater = require('./lib/populater')
-let connObj
+const app = express()
 
 let connString = 'pg://' + config.db.user + ':' + config.db.password + '@' + config.db.host + ':' + config.db.port + '/' + config.db.name
 let pgPool = new pg.Pool({
@@ -13,16 +10,8 @@ let pgPool = new pg.Pool({
 pgPool.connect((err, client) => {
   if (err) console.log('Unable to connect: ' + err)
   app.use(express.static('public'))
-  connObj = client
-  helper(populater, execQuery)
 })
 
-const execQuery = (query, values) => {
-  connObj.query(query, values, (err, res) => {
-    if (err) console.error(err)
-    else console.log(res.rows.length)
-  })
-}
 app.listen(config.app.port, () => {
   console.log('Listening on port: ' + config.app.port)
 })
