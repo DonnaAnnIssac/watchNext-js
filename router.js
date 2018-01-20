49cadca9b3a68868be8b129c2b74965249d22c15
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-20 18:23:46 +0530

    Add route for fetching movies by genre

--- router.js
+++ router.js
@@ -3,5 +3,6 @@ const movie = require('./controllers/movieController')
 const routes = express.Router()
 
 routes.get('/movie', movie.get)
+routes.get('/genre', movie.getByGenre)
 
 module.exports = routes
