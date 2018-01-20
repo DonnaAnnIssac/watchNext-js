e9027e295d731e744eb918f8d6ad49b089dd8697
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-20 18:47:29 +0530

    Added method to get latest movies

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,22 +2,26 @@ const db = require('../db')
 let movieController = {}
 
 movieController.get = (req, res) => {
-  console.log(req.query.title)
   let value = '%' + req.query.title.toUpperCase() + '%'
   let query = 'SELECT * FROM movies where primary_title like $1;'
   db.query(query, [value], (err, response) => {
     if (err) console.error(err)
-    console.log(response.rows)
     res.json(response.rows)
   })
 }
 
 movieController.getByGenre = (req, res) => {
-  console.log(req.query.genre)
   let query = 'select * from movies where tconst in (select tconst from genres where genre = $1);'
   db.query(query, [req.query.genre], (err, response) => {
     if (err) console.error(err)
-    console.log(response.rows)
+    res.json(response.rows)
+  })
+}
+
+movieController.getByYear = (req, res) => {
+  let query = 'select * from movies order by start_year desc;'
+  db.query(query, (err, response) => {
+    if (err) console.error(err)
     res.json(response.rows)
   })
 }
