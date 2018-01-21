c278493ebc9ecc2c913674abea9a79322aabbac3
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-21 09:48:48 +0530

    Add method to get top rated movies

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,26 +3,41 @@ let movieController = {}
 
 movieController.get = (req, res) => {
   let value = '%' + req.query.title.toUpperCase() + '%'
-  let query = 'SELECT * FROM movies where primary_title like $1;'
-  db.query(query, [value], (err, response) => {
+  let q1 = 'select movies.tconst, movies.primary_title, movies.start_year, movies.runtime_mins, ratings.average_rating from movies'
+  let q2 = ' inner join ratings on movies.tconst = ratings.tconst'
+  let q3 = ' where primary_title like $1;'
+  db.query(q1 + q2 + q3, [value], (err, response) => {
     if (err) console.error(err)
     res.json(response.rows)
   })
 }
 
 movieController.getByGenre = (req, res) => {
-  let query = 'select * from movies where tconst in (select tconst from genres where genre = $1);'
-  db.query(query, [req.query.genre], (err, response) => {
+  let q1 = 'select movies.tconst, movies.primary_title, movies.start_year, movies.runtime_mins, ratings.average_rating from movies'
+  let q2 = ' inner join ratings on movies.tconst = ratings.tconst'
+  let q3 = ' where movies.tconst in (select tconst from genres where genre = $1);'
+  db.query(q1 + q2 + q3, [req.query.genre], (err, response) => {
     if (err) console.error(err)
     res.json(response.rows)
   })
 }
 
 movieController.getByYear = (req, res) => {
-  let query = 'select * from movies order by start_year desc;'
-  db.query(query, (err, response) => {
+  let q1 = 'select movies.tconst, movies.primary_title, movies.start_year, movies.runtime_mins, ratings.average_rating from movies'
+  let q2 = ' inner join ratings on movies.tconst = ratings.tconst order by movies.start_year desc;'
+  db.query(q1 + q2, (err, response) => {
     if (err) console.error(err)
     res.json(response.rows)
   })
 }
+
+movieController.getByRating = (req, res) => {
+  let q1 = 'select movies.tconst, movies.primary_title, movies.start_year, movies.runtime_mins, ratings.average_rating from movies'
+  let q2 = ' inner join ratings on movies.tconst = ratings.tconst order by ratings.average_rating desc;'
+  db.query(q1 + q2, (err, response) => {
+    if (err) console.error(err)
+    res.json(response.rows)
+  })
+}
+
 module.exports = movieController
