21dffc940b75425269dcfe367d5e74ee0b63afcb
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-21 12:25:25 +0530

    Add methods to get movies by director and most popular movies

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -40,4 +40,22 @@ movieController.getByRating = (req, res) => {
   })
 }
 
+movieController.getByPopularity = (req, res) => {
+  let q1 = 'select movies.tconst, movies.primary_title, movies.start_year, movies.runtime_mins, ratings.average_rating from movies'
+  let q2 = ' inner join ratings on movies.tconst = ratings.tconst order by num_votes desc;'
+  db.query(q1 + q2, (err, response) => {
+    if (err) console.error(err)
+    res.json(response.rows)
+  })
+}
+
+movieController.getByDirector = (req, res) => {
+  let q1 = 'select * from movies where tconst in ('
+  let q2 = 'select tconst from directors where nconst in ('
+  let q3 = 'select nconst from celebs where primary_name = $1));'
+  db.query(q1 + q2 + q3, [req.query.director.toUpperCase()], (err, response) => {
+    if (err) console.error(err)
+    res.json(response.rows)
+  })
+}
 module.exports = movieController
