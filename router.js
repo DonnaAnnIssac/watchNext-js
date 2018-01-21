07ca0a100fd72c86f6c7b5e0d4a9e159f421117d
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-21 09:50:38 +0530

    Add route to get top rated movies;

--- router.js
+++ router.js
@@ -3,6 +3,8 @@ const movie = require('./controllers/movieController')
 const routes = express.Router()
 
 routes.get('/movie', movie.get)
-routes.get('/genre', movie.getByGenre)
+routes.get('/movie/genre', movie.getByGenre)
+routes.get('/movie/release', movie.getByYear)
+routes.get('/movie/rating', movie.getByRating)
 
 module.exports = routes
