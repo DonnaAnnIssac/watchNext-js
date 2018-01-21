1fadd93829cad893eeb3f3f4768b4f2fce0faa0a
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-21 12:42:12 +0530

    Add route to get info about celebs

--- router.js
+++ router.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const movie = require('./controllers/movieController')
+const celeb = require('./controllers/celebController')
 const routes = express.Router()
 
-routes.get('/movie', movie.get)
+routes.get('/movie', movie.getMovie)
 routes.get('/movie/genre', movie.getByGenre)
 routes.get('/movie/release', movie.getByYear)
 routes.get('/movie/rating', movie.getByRating)
+routes.get('/movie/popular', movie.getByPopularity)
+routes.get('/movie/director', movie.getByDirector)
+routes.get('/celeb', celeb.getInfo)
 
 module.exports = routes
